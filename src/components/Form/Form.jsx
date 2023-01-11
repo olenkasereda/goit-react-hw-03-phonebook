cacf1df1dab2dcf3966b1a30ad0f9353337fa34b
olenkasereda <lenuhasereda@gmail.com>
2023-01-11 17:01:59 +0200

    localStorage

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,72 @@
+import { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import s from './Form.module.css';
+
+class Form extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = evt => {
+    const { name, value } = evt.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    const { name, number } = this.state;
+    this.props.addContact({ name: name, number: number });
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    const { name, number } = this.state;
+
+    return (
+      <form className={s.form} onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input
+            placeholder="Full name"
+            type="text"
+            name="name"
+            value={name}
+            pattern="^[a-zA-Za-яА-Я]+(([' -][a-zA-Za-яА-Я ])?[a-zA-Za-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+
+        <label>
+          Number
+          <input
+            placeholder="Phone number: +380..."
+            type="tel"
+            name="number"
+            value={number}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+        <br />
+        <button className={s.buttonAdd} type="submit">
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+Form.propTypes = {
+  addContact: PropTypes.func.isRequired,
+};
+
+export default Form;
